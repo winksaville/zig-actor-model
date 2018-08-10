cbbce098319320c59b916ba1b8ff68293350c15c
Wink Saville <wink@saville.com>
2018-08-10 08:49:57 -0700

    Add test ActorDispatcher
    This is from test.zig

--- actor_dispatcher.zig
+++ actor_dispatcher.zig
@@ -1,18 +1,23 @@
 // Actor dispatcher
 
-const ActorInterface = @import("actor.zig").ActorInterface;
-const MessageHeader = @import("message.zig").MessageHeader;
-const AtomicOrder = builtin.AtomicOrder;
-const AtomicRmwOp = builtin.AtomicRmwOp;
+const actorNs = @import("actor.zig");
+const ActorInterface = actorNs.ActorInterface;
+
+const messageNs = @import("message.zig");
+const MessageHeader = messageNs.MessageHeader;
+
+const messageQueueNs = @import("message_queue.zig");
+const MessageQueue = messageQueueNs.MessageQueue;
+const SignalContext = messageQueueNs.SignalContext;
 
 const builtin = @import("builtin");
 const std = @import("std");
 const assert = std.debug.assert;
 const warn = std.debug.warn;
-const MQ = @import("message_queue.zig");
-const MessageQueue = MQ.MessageQueue;
-const SignalContext = MQ.SignalContext;
+
 const futex_wake = @import("futex.zig").futex_wake;
+const AtomicOrder = builtin.AtomicOrder;
+const AtomicRmwOp = builtin.AtomicRmwOp;
 
 /// Dispatches messages to actors
 pub fn ActorDispatcher(comptime maxActors: usize) type {
@@ -79,4 +84,107 @@ pub fn ActorDispatcher(comptime maxActors: usize) type {
             }
         }
     };
+}
+
+// Tests
+
+const Message = messageNs.Message;
+const Actor = actorNs.Actor;
+const mem = std.mem;
+
+const MyMsgBody = packed struct {
+    const Self = this;
+    data: [3]u8,
+
+    fn init(pSelf: *Self) void {
+        mem.set(u8, pSelf.data[0..], 'Z');
+    }
+
+    pub fn format(
+        m: *const MyMsgBody,
+        comptime fmt: []const u8,
+        context: var,
+        comptime FmtError: type,
+        output: fn (@typeOf(context), []const u8) FmtError!void
+    ) FmtError!void {
+        try std.fmt.format(context, FmtError, output, "data={{");
+        for (m.data) |v| {
+            if ((v >= ' ') and (v <= 0x7f)) {
+                try std.fmt.format(context, FmtError, output, "{c}," , v);
+            } else {
+                try std.fmt.format(context, FmtError, output, "{x},", v);
+            }
+        }
+        try std.fmt.format(context, FmtError, output, "}},");
+    }
+};
+
+const MyActorBody = packed struct {
+    const Self = this;
+
+    count: u64,
+
+    fn init(actr: *Actor(MyActorBody)) void {
+        actr.body.count = 0;
+    }
+
+    pub fn processMessage(actorInterface: *ActorInterface, msgHeader: *MessageHeader) void {
+        var pActor = Actor(MyActorBody).getActorPtr(actorInterface);
+        var pMsg = Message(MyMsgBody).getMessagePtr(msgHeader);
+        assert(pMsg.header.cmd == msgHeader.cmd);
+
+        pActor.body.count += pMsg.header.cmd;
+        //warn("MyActorBody: &processMessage={x} cmd={} count={}\n",
+        //    @ptrToInt(processMessage), msgHeader.cmd, pActor.body.count);
+    }
+};
+
+test "ActorDispatcher" {
+    // Create a message
+    const MyMsg = Message(MyMsgBody);
+    var myMsg = MyMsg.init(123);
+
+    //// Create a queue of MessageHeader pointers
+    //const MyQueue = Queue(*MessageHeader);
+    //var q = MyQueue.init();
+
+    //// Create a node with a pointer to a message header
+    //var node_0 = MyQueue.Node {
+    //    .data = &myMsg.header,
+    //    .next = undefined,
+    //};
+
+    //// Add and remove it from the queue and verify
+    //q.put(&node_0);
+    //var n = q.get() orelse { return error.QGetFailed; };
+    //var pMsg = Message(MyMsgBody).getMessagePtr(n.data);
+
+    // Create an Actor
+    const MyActor = Actor(MyActorBody);
+    var myActor = MyActor.init();
+
+    myActor.interface.processMessage(&myActor.interface, &myMsg.header);
+    assert(myActor.body.count == 1 * 123);
+    myActor.interface.processMessage(&myActor.interface, &myMsg.header);
+    assert(myActor.body.count == 2 * 123);
+
+    const MyActorDispatcher = ActorDispatcher(5);
+    var myActorDispatcher: MyActorDispatcher = undefined;
+    myActorDispatcher.init();
+    assert(myActorDispatcher.actors_count == 0);
+    try myActorDispatcher.add(&myActor.interface);
+    assert(myActorDispatcher.actors_count == 1);
+    assert(myActorDispatcher.actors[0].processMessage == myActor.interface.processMessage);
+
+    const Msg0 = Message(packed struct {});
+    var msg0: Msg0 = undefined;
+    msg0.header.init(123);
+
+    // Place the node on the queue and broadcast to the actors
+    myActorDispatcher.queue.put(&msg0.header);
+    myActorDispatcher.broadcastLoop();
+    assert(myActorDispatcher.last_msg_cmd == 123);
+    assert(myActorDispatcher.msg_count == 1);
+    assert(myActorDispatcher.actor_processMessage_count == 1);
+    assert(myActor.body.count == 3 * 123);
 }
