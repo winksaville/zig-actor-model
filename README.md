46d6aa1738f5b26d63d606d3cc8e02cf493185d2
Wink Saville <wink@saville.com>
2018-08-16 13:55:12 -0700

    Test two different ways of using futex.
    When mode is naive there is no optimization and it takes about 30 seconds
    on my desktop computer. If naive is NOT enabled it's about 10x faster at
    about 3 seconds. This is "debug" compile mode.
    
    In --release-fast mode it takes 25 seconds! The reason seems to be that
    the "stall" loops were optimized away.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ based on the zig-simple-actor code and uses the
 "interface" model to allow generic Actors and
 generic Messages.
 
-Minimally functional at this point.
+Somewhat functional at this point.
 
 ## Test
 ```bash
@@ -37,3 +37,43 @@ Remove `zig-cache/` directory
 ```bash
 $ rm -rf ./zig-cache/
 ```
+## Notes
+### Futex
+
+On my desktop when mode is naive and its a debug build takes about 30 seconds:
+```bash
+$ zig test futex.zig
+Test 1/1 Futex...
+test Futex:+ gCounter=0 gProducerWaitCount=0 gConsumerWaitCount=0 gProducerWakeCount=0 gConsuerWakeCount=0
+test Futex: time=28.671786
+test Futex:- gCounter=20000000 gProducerWaitCount=6520118 gConsumerWaitCount=6521602 gProducerWakeCount=10000000 gConsuerWakeCount=10000000
+OK
+All tests passed.
+```
+
+if mode is !naive is about 10x faster at about 3 seconds.
+```bash
+$ zig test futex.zig
+Test 1/1 Futex...test Futex:+ gCounter=0 gProducerWaitCount=0 gConsumerWaitCount=0 gProducerWakeCount=0 gConsuerWakeCount=0
+test Futex: time=2.930429
+test Futex:- gCounter=20000000 gProducerWaitCount=26 gConsumerWaitCount=10 gProducerWakeCount=143 gConsuerWakeCount=178
+OK
+All tests passed.
+```
+
+But with a --release-fast and mode is !naive it takes about 25 seconds:
+```bash
+$ zig test --release-fast futex.zig
+Test 1/1 Futex...
+test Futex:+ gCounter=0 gProducerWaitCount=0 gConsumerWaitCount=0 gProducerWakeCount=0 gConsuerWakeCount=0
+test Futex: time=24.833821
+test Futex:- gCounter=20000000 gProducerWaitCount=5804057 gConsumerWaitCount=5813210 gProducerWakeCount=9999938 gConsuerWakeCount=9999952
+OK
+All tests passed.
+```
+
+I looked at the assembler output with `objdump --source -d -M intel zig-cache/test > test.fast.asm` and
+found the code at `<MainFuncs_linuxThreadMain>:`. In that routine we see both the producer code
+followed by the consumer at `<MainFuncs_linuxThreadMain.21>:`. Comparing the output to `test.debug.asm`
+it appears to me the compiler optimized away the "stall" loops. I saw similar times with --release-small (26s)
+and --release-safe (28s).
--- futex.zig
+++ futex.zig
@@ -4,6 +4,7 @@ const warn = std.debug.warn;
 const mem = std.mem;
 const math = std.math;
 const Queue = std.atomic.Queue;
+const Timer = std.os.time.Timer;
 
 const builtin = @import("builtin");
 const AtomicOrder = builtin.AtomicOrder;
@@ -21,12 +22,12 @@ pub use switch(builtin.arch) {
 
 
 
-pub fn futex_wait(pVal: *u32, expected_value: u32) void {
+pub fn futex_wait(pVal: *i32, expected_value: u32) void {
     //warn("futex_wait: {*}\n", pVal);
     _ = syscall4(SYS_futex, @ptrToInt(pVal), linux.FUTEX_WAIT, expected_value, 0);
 }
 
-pub fn futex_wake(pVal: *u32, num_threads_to_wake: u32) void {
+pub fn futex_wake(pVal: *i32, num_threads_to_wake: u32) void {
     //warn("futex_wake: {*}\n", pVal);
     _ = syscall4(SYS_futex, @ptrToInt(pVal), linux.FUTEX_WAKE, num_threads_to_wake, 0);
 }
@@ -35,15 +36,11 @@ pub fn futex_wake(pVal: *u32, num_threads_to_wake: u32) void {
 const ThreadContext = struct {
     const Self = this;
 
-    name_len: usize,
-    name: [32]u8,
+    mode: bool,
     counter: u128,
 
-    pub fn init(pSelf: *Self, name: [] const u8) void {
-        // Set name_len and then copy with truncation
-        pSelf.name_len = math.min(name.len, pSelf.name.len);
-        mem.copy(u8, pSelf.name[0..pSelf.name_len], name[0..pSelf.name_len]);
-
+    pub fn init(pSelf: *Self, mode: bool) void {
+        pSelf.mode = mode;
         pSelf.counter = 0;
     }
 };
@@ -51,55 +48,152 @@ const ThreadContext = struct {
 var gProducer_context: ThreadContext = undefined;
 var gConsumer_context: ThreadContext = undefined;
 
-var produce: u32 = 0;
-var gCounter: u128 = 0;
+const naive = true;
+const consumeSignal = 0;
+const produceSignal = 1;
+var produce: i32 = consumeSignal;
+var gCounter: u64 = 0;
+var gProducerWaitCount: u64 = 0;
+var gConsumerWaitCount: u64 = 0;
+var gProducerWakeCount: u64 = 0;
+var gConsumerWakeCount: u64 = 0;
+
+const max_counter = 10000000;
+const stallCountWait: u32 = 1000;
+const stallCountWake: u32 = 200;
 
 fn producer(pContext: *ThreadContext) void {
-    while (pContext.counter < 1000000) {
-        // Wait for consumer to request production
-        while (@atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst) == 0) {
-            futex_wait(&produce, 0);
+    if (pContext.mode == naive) {
+        while (pContext.counter < max_counter) {
+            // Wait for the produce to be the produceSignal
+            while (@atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst) != produceSignal) {
+                gProducerWaitCount += 1;
+                futex_wait(&produce, consumeSignal);
+            }
+
+            // Produce
+            gCounter += 1;
+            pContext.counter += 1;
+
+            // Tell consumer to consume and then wake consumer up
+            _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, consumeSignal, AtomicOrder.SeqCst);
+            gProducerWakeCount += 1;
+            futex_wake(&produce, 1);
         }
+    } else {
+        while (pContext.counter < max_counter) {
+            // Wait for the produce to be the produceSignal
+            var count = stallCountWait;
+            var produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            while ((produce_val != produceSignal) and (count > 0)) {
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+                count -= 1;
+            }
+            while (produce_val != produceSignal) {
+                gProducerWaitCount += 1;
+                futex_wait(&produce, consumeSignal);
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            }
 
-        // Produce
-        gCounter += 1;
-        pContext.counter += 1;
+            // Produce
+            gCounter += 1;
+            pContext.counter += 1;
 
-        // Tell consumer we produced with "store 0"
-        _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, 0, AtomicOrder.SeqCst);
-        futex_wake(&produce, 1);
+            // Tell consumer to consume
+            _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, consumeSignal, AtomicOrder.SeqCst);
+
+            // Wake up consumer if needed
+            count = stallCountWake;
+            produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            while ((produce_val == consumeSignal) and (count > 0)) {
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+                count -= 1;
+            }
+            if (produce_val == consumeSignal) {
+                gProducerWakeCount += 1;
+                futex_wake(&produce, 1);
+            }
+        }
     }
 }
 
 fn consumer(pContext: *ThreadContext) void {
-    while (pContext.counter < 1000000) {
-        // Tell producer to produce with "store 1"
-        _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, 1, AtomicOrder.SeqCst);
-        futex_wake(&produce, 1);
+    if (pContext.mode == naive) {
+        while (pContext.counter < max_counter) {
+            // Tell producer to produce
+            _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, produceSignal, AtomicOrder.SeqCst);
+            gConsumerWakeCount += 1;
+            futex_wake(&produce, 1);
 
-        // Wait until something is produced by seeing a 0
-        while (@atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst) == 1) {
-            futex_wait(&produce, 1);
+            // Wait for producer to produce
+            while (@atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst) == produceSignal) {
+                gConsumerWaitCount += 1;
+                futex_wait(&produce, produceSignal);
+            }
+
+            // Consume
+            gCounter += 1;
+            pContext.counter += 1;
         }
+    } else {
+        while (pContext.counter < max_counter) {
+            // Tell producer to produce
+            _ = @atomicRmw(@typeOf(produce), &produce, AtomicRmwOp.Xchg, produceSignal, AtomicOrder.SeqCst);
 
-        // Consume
-        gCounter += 1;
-        pContext.counter += 1;
+            // Wake up producer if needed
+            var count = stallCountWake;
+            var produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            while ((produce_val == produceSignal) and (count > 0)) {
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+                count -= 1;
+            }
+            if (produce_val == produceSignal) {
+                gConsumerWakeCount += 1;
+                futex_wake(&produce, 1);
+            }
+
+            // Wait for producer to produce
+            count = stallCountWait;
+            produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            while ((produce_val == produceSignal) and (count > 0)) {
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+                count -= 1;
+            }
+            while (produce_val == produceSignal) {
+                gConsumerWaitCount += 1;
+                futex_wait(&produce, produceSignal);
+                produce_val = @atomicLoad(@typeOf(produce), &produce, AtomicOrder.SeqCst);
+            }
+
+            // Consume
+            gCounter += 1;
+            pContext.counter += 1;
+        }
     }
 }
 
 test "Futex" {
-    warn("\ntest Futex:+ gCounter={}\n", gCounter);
-    defer warn("test Futex:- gCounter={}\n", gCounter);
+    warn("\ntest Futex:+ gCounter={} gProducerWaitCount={} gConsumerWaitCount={} gProducerWakeCount={} gConsuerWakeCount={}\n",
+        gCounter, gProducerWaitCount, gConsumerWaitCount, gProducerWakeCount, gConsumerWakeCount);
+    defer warn("test Futex:- gCounter={} gProducerWaitCount={} gConsumerWaitCount={} gProducerWakeCount={} gConsuerWakeCount={}\n",
+        gCounter, gProducerWaitCount, gConsumerWaitCount, gProducerWakeCount, gConsumerWakeCount);
 
-    gProducer_context.init("producer");
-    gConsumer_context.init("consumer");
+    gProducer_context.init(!naive);
+    gConsumer_context.init(!naive);
+
+    var timer = try Timer.start();
+
+    var start_time = timer.read();
+
     var producerThread = try std.os.spawnThread(&gProducer_context, producer);
     var consumerThread = try std.os.spawnThread(&gConsumer_context, consumer);
 
     producerThread.wait();
     consumerThread.wait();
-    warn("test Futex: gCounter={}\n", gCounter);
 
-    assert(gCounter == 2000000);
+    var end_time = timer.read();
+    var duration = end_time - start_time;
+    warn("test Futex: time={.6}\n", @intToFloat(f64, end_time - start_time) / @intToFloat(f64, std.os.time.ns_per_s));
+
+    assert(gCounter == max_counter * 2);
 }
